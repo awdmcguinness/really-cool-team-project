2eb0dd6c44e1c2b149c0d82f5f9564a96f2d31a9
Pamela Vasquez <pamelavasquez94@gmail.com>
2019-12-14 09:56:45 -0500

    DOCS: removed extra code

--- weather.js
+++ weather.js
@@ -5,11 +5,3 @@ $.ajax(weatherURL).done(function(response) {
   console.log(response);
   console.log(response.current.temp_f);
 });
-
-var twitterURL =
-  "http://api.twitter.com/1.0/statuses/user_timeline.json?screen_name=twitterapi&count=2";
-
-$.ajax(twitterURL).done(function(response) {
-  console.log(response);
-  console.log(response.current.temp_f);
-});
