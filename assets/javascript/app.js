1b95d4b951e983920d9a917ebbee039688fc4074
Anthony McGuinness <amcguinness.re@gmail.com>
2019-12-14 13:02:18 -0500

    created initial files for the chat also edited file paths

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 // Click events for index.html train images (1, 2, 3)
 
+var selectedLine = "";
+
 $("#oneTrain").click(function() {
   $("#oneTrainCard").css("display", "block");
   $(".numberChoices").css("display", "none");
@@ -20,16 +22,19 @@ $("#threeTrain").click(function() {
 $("#northbound-one").click(function() {
   $("#oneTrainCard").css("display", "none");
   $("#messageCardOneTrain").css("display", "block");
+  selectedLine = "one-northbound";
 });
 
 $("#northbound-two").click(function() {
   $("#twoTrainCard").css("display", "none");
   $("#messageCardTwoTrain").css("display", "block");
+  selectedLine = "two-northbound";
 });
 
 $("#northbound-three").click(function() {
   $("#threeTrainCard").css("display", "none");
   $("#messageCardThreeTrain").css("display", "block");
+  selectedLine = "three-northbound";
 });
 
 // Click events on "Southbound" clicks
@@ -37,14 +42,17 @@ $("#northbound-three").click(function() {
 $("#southbound-one").click(function() {
   $("#oneTrainCard").css("display", "none");
   $("#messageCardOneTrain").css("display", "block");
+  selectedLine = "one-southbound";
 });
 
 $("#southbound-two").click(function() {
   $("#twoTrainCard").css("display", "none");
   $("#messageCardTwoTrain").css("display", "block");
+  selectedLine = "two-southbound";
 });
 
 $("#southbound-three").click(function() {
   $("#threeTrainCard").css("display", "none");
   $("#messageCardThreeTrain").css("display", "block");
+  selectedLine = "three-southbound";
 });
